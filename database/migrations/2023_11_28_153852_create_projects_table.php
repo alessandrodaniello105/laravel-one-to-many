32d82e64bbfe9f2b60b5ac920233b7b8d0c457de
alessandrodaniello105 <kanymsevement@gmail.com>
2023-12-01 15:25:32 +0100

    [del]: old 'type' from create_projects_table

--- database/migrations/2023_11_28_153852_create_projects_table.php
+++ database/migrations/2023_11_28_153852_create_projects_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('title', 75);
             $table->string('slug', 76);
             $table->string('description', 255);
-            $table->string('type', 15);
             $table->timestamps();
         });
     }
