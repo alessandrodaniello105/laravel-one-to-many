96c005e8598e99f2be28ffcdaed44bb25f6aaa9e
alessandrodaniello105 <kanymsevement@gmail.com>
2023-12-01 15:27:06 +0100

    [add]: type_id FK in Projects seeder

--- database/seeders/ProjectsTableSeeder.php
+++ database/seeders/ProjectsTableSeeder.php
@@ -8,6 +8,7 @@ use App\Models\Project;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 use App\Models\Technology;
+use App\Models\Type;
 
 class ProjectsTableSeeder extends Seeder
 {
@@ -22,10 +23,10 @@ class ProjectsTableSeeder extends Seeder
             $new_project = new Project();
             //utilizzo il faker per vedere se funziona il seeder
             $new_project->technology_id = Technology::inRandomOrder()->first()->id;
+            $new_project->type_id = Type::inRandomOrder()->first()->id;
             $new_project->title = $faker->text(75);
             $new_project->slug = Str::slug($new_project->title, '-');
             $new_project->description = $faker->paragraph(2);
-            $new_project->type = $faker->randomElement(['public','private']);
             $new_project->save();
         }
     }
