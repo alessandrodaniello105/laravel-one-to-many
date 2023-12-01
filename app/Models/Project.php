633d70ecab29c77d24e029d2f515d3f9c50dc69a
alessandrodaniello105 <kanymsevement@gmail.com>
2023-12-01 15:08:53 +0100

    [add]: type relationship in Project+Type model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -15,6 +15,10 @@ class Project extends Model
         return $this->belongsTo(Technology::class);
     }
 
+    public function type() {
+        return $this->belongsTo(Type::class);
+    }
+
     public static function generateSlug($string) {
         $slug = Str::slug($string, '-');
         $original_slug = $slug;
