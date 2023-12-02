903d8a457f8983fd1948d1d1660c16636ffc2469
alessandrodaniello105 <kanymsevement@gmail.com>
2023-12-02 19:15:12 +0100

    [add]: input form for new technology in its index

--- resources/views/admin/technologies/index.blade.php
+++ resources/views/admin/technologies/index.blade.php
@@ -4,14 +4,33 @@
 
     <h1>Lista Tecnologie</h1>
 
-    <ul>
-        @foreach ($technologies as $technology)
-        <li>
-            <a href="{{$technology->link}}">{{$technology->name}}</a>
-        </li>
-        @endforeach
+    <div class="container my-5">
 
-    </ul>
+    <form action="{{ route('admin.technologies.store') }}" method="POST">
+        @csrf
+
+            <div class="input-group mb-3">
+
+                <input type="text" class="form-control" placeholder="Nome nuova tecnologia" id="new-technology-input" name="name">
+
+                <button type="submit" class="btn btn-warning">Invia</button>
+
+            </div>
+
+
+        </form>
+
+        <ul>
+            @foreach ($technologies as $technology)
+            <li>
+                <a href="{{$technology->link}}">{{$technology->name}}</a>
+            </li>
+            @endforeach
+
+        </ul>
+
+    </div>
+
 
 @endsection
 
