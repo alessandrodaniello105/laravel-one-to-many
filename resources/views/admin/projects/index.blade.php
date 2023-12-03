078fe56d97887c0f5393e9c2b88bcbf71dea3e16
alessandrodaniello105 <kanymsevement@gmail.com>
2023-12-03 23:16:51 +0100

    [edit]: del button is now by include in projects

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -48,11 +48,7 @@
                                 </a>
 
                                 {{-- DELETE BUTTON --}}
-                                <form class="d-inline-block" method="POST" action="{{route('admin.projects.destroy', $project)}}">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-danger"><i class="fa-solid fa-eraser"></i></button>
-                                </form>
+                                @include('admin.partials.formDelete', ["route" => route('admin.projects.destroy', $project)])
 
                             </div>
 
